e3ac43052b92a0dd5ec5587832dfb0fbc0006b96
zawlwin223 <zawlwinp223@gmail.com>
2024-12-20 20:17:10 -0800

    cartUi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+import CartProvider from './store/CartProvider'
+import Cart from './components/Cart/Cart'
 
 function App() {
   return (
     <CartProvider>
+      <Cart></Cart>
       <Header />
       <main>
         <Meals />
       </main>
     </CartProvider>
-  );
+  )
 }
 
-export default App;
+export default App
